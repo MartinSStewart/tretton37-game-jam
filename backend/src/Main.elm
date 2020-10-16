8d116a2e74efab567a1af4e33eb4d23f34475f44
Unknown <MartinSStewart@gmail.com>
2020-10-16 21:31:28 +0200

    Change to migration version

--- backend/src/Main.elm
+++ backend/src/Main.elm
@@ -1,20 +1,25 @@
 module Main exposing (Msg(..), main, update)
 
+import Bytes.Encode
 import Codec.Bytes exposing (Codec)
-import Dict exposing (Dict)
 import Http.Server.LowLevel as HSL
 import MainLogic exposing (FrontendMsg(..), Model)
 import Simplex
 
 
 type Msg
+    = GetRequest_ HSL.HttpRequest
+    | NoOp
+
+
+type OldMsg
     = GetRequest HSL.HttpRequest
 
 
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
     case msg of
-        GetRequest req ->
+        GetRequest_ req ->
             case Codec.Bytes.decodeValue MainLogic.frontendMsgCodec req.body of
                 Just RequestHighscores ->
                     ( model
@@ -43,12 +48,28 @@ update msg model =
                     )
 
                 Nothing ->
-                    ( model, Cmd.none )
+                    ( model
+                    , HSL.respond
+                        { requestId = req.requestId
+                        , status = 200
+                        , headers = [ ( "Content-Type", "text/plain" ) ]
+                        , body = Bytes.Encode.string "Bad body" |> Bytes.Encode.encode
+                        }
+                    )
+
+        NoOp ->
+            ( model, Cmd.none )
 
 
+init : Model
+init =
+    { highscores = [] }
+
+
+main : Simplex.BackendProgram () Model Msg ( OldMsg, Model )
 main =
-    Simplex.new
-        { init = \() -> ( { highscores = [] }, Cmd.none )
-        , update = update
-        , subscriptions = \m -> HSL.subscribe GetRequest
+    Simplex.zeroDowntimeMigration
+        { update = update
+        , subscriptions = \m -> HSL.subscribe GetRequest_
+        , migrate = { model = \_ _ -> Ok init, msg = \_ _ -> Ok NoOp }
         }
